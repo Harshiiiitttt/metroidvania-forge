8464740b428f1f2b838d8e29d6a31d4a1a1c77a3
Harshiiiitttt <harshitkudaliya99@gmail.com>
2025-12-11 21:21:13 +0530

    idle/run

---
+++ player/scripts/player.gd
@@ -0,0 +1,90 @@
+class_name Player extends CharacterBody2D
+
+
+#region /// State Machine Variables
+var states : Array[PlayerState]
+var current_state : PlayerState :
+	get : return states.front() #front returns the first element of the array
+var previous_state : PlayerState :
+	get : return states[1]
+#endregion
+
+#region /// standard variables
+var directions : Vector2 = Vector2(0, 0) #or Vector2.ZERO
+var gravity : float = 980
+#endregion
+
+
+
+func _ready() -> void:
+	initialize_states()
+	pass
+
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	change_state(current_state.handle_input(event))
+	pass
+
+
+
+func _process(_delta: float) -> void:
+	change_state( current_state.process( _delta ) )
+	pass
+
+
+
+func _physics_process(_delta: float) -> void:
+	velocity.y += gravity * _delta
+	move_and_slide()
+	change_state( current_state.physics_process( _delta ) )
+	pass
+
+
+
+func initialize_states() -> void:
+	states = []
+	#gather all the states
+	for c in $States.get_children():
+		if c is PlayerState:
+			states.append(c)
+			c.player = self # self refers to the PLayer class
+		pass
+		
+	if states.size() == 0:
+		return
+		 
+	#initialize all states
+	for state in states:
+		state.init()
+		
+	change_state(current_state)
+	current_state.enter()
+	$Label.text = current_state.name
+	pass
+	
+	 
+	
+func change_state(new_state : PlayerState) -> void:
+	if new_state == null:
+		return
+	elif new_state == current_state:
+		return
+		
+	if current_state:
+		current_state.exit()
+		
+	states.push_front(new_state)
+	current_state.enter()
+	states.resize(3) #it will only keep 3 items and will remove the rest
+	$Label.text = current_state.name
+	pass
+
+
+
+func update_direction() -> void:
+	#var prev_direction : Vector2 = directions
+	var x_axis = Input.get_axis("left", "right")
+	var y_axis = Input.get_axis("up", "down")
+	directions = Vector2(x_axis, y_axis)
+	pass
---
+++ player/states/run.gd
@@ -0,0 +1,35 @@
+class_name PlayerStateRun extends PlayerState
+
+
+#  What happens when the state is initialized?
+func init() -> void:
+	pass
+	
+	
+# what happens when we enter this state?
+func enter() -> void:
+	#play animation
+	pass
+
+
+# what happens when we exit this state?
+func exit() -> void:
+	pass
+	
+
+# What happens when an input is pressed?
+func handle_input(_event : InputEvent) -> PlayerState:
+	#handle inputs 
+	return next_state
+	
+	
+# What happens each process tick in this state?
+func process(_delta: float) -> PlayerState:
+	if player.directions.x == 0:
+		return idle
+	return next_state
+	
+	
+# What happens each physiscs_process tick in this state?
+func physics_process(_delta: float) -> PlayerState:
+	return next_state
